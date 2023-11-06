d2530a5369a010c386bb9d11e9cb50e69209e37f
Luka Kuterovac <luka.kuterovac@gmail.com>
2023-11-06 18:36:32 +0100

    Create a Read-Only Web API

---
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using ContactManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using ContactManager.Services;
+
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
+        // GET: Contact
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+
+    }
+}
